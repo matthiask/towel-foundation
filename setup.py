54d7256aab75fdb070a92515209fb40737221f25
Matthias Kestenholz <mk@spinlock.ch>
2012-07-11 11:50:00 +0200

    towel_bootstrap

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import os
+import setuplib
+
+packages, package_data = setuplib.find_packages('towel_bootstrap')
+
+setup(name='towel_bootstrap',
+    version=__import__('towel_bootstrap').__version__,
+    description='Keeping you DRY since 2010',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README')).read().decode('utf-8'),
+    author='Matthias Kestenholz',
+    author_email='mk@feinheit.ch',
+    url='http://github.com/matthiask/towel_bootstrap/',
+    license='BSD License',
+    platforms=['OS Independent'],
+    packages=packages,
+    package_data=package_data,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+)
