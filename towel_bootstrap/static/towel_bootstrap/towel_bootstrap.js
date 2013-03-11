f4f24d8badb0bc818d04d575f8f1121465eee120
Matthias Kestenholz <mk@spinlock.ch>
2013-03-11 10:01:04 +0100

    I do not like the multiple chosen widget too much

--- towel_bootstrap/static/towel_bootstrap/towel_bootstrap.js
+++ towel_bootstrap/static/towel_bootstrap/towel_bootstrap.js
@@ -44,7 +44,7 @@ if (window.gettext === undefined) {
 
     onForm.push(function(elem, $) {
         if ($.fn.chosen)
-            elem.find('select').chosen();
+            elem.find('select').not('[multiple]').chosen();
         if ($.fn.datepicker) {
             elem.find('.date').each(function() {
                 var self = $(this),
