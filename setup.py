39862d6ddda21277783825437d417d0a334ab876
Matthias Kestenholz <mk@spinlock.ch>
2013-08-26 17:48:31 +0200

    flake8 the code a bit

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ packages, package_data = setuplib.find_packages('towel_foundation')
 setup(name='towel-foundation',
     version=__import__('towel_foundation').__version__,
     description='Keeping you DRY since 2013',
-    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    long_description=open(
+        os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
     author='Matthias Kestenholz',
     author_email='mk@feinheit.ch',
     url='http://github.com/matthiask/towel-foundation/',
--- towel_foundation/templatetags/foundation_tags.py
+++ towel_foundation/templatetags/foundation_tags.py
@@ -1,11 +1,12 @@
 from django import template
-from django.conf import settings
 from django.utils.formats import get_format
+
 
 register = template.Library()
 
+
 @register.simple_tag
 def date_format():
-    format = get_format('DATE_INPUT_FORMATS')[0]
-    format = format.replace('%m', 'mm').replace('%d', 'dd').replace('%Y', 'yyyy')
-    return format
+    fmt = get_format('DATE_INPUT_FORMATS')[0]
+    fmt = fmt.replace('%m', 'mm').replace('%d', 'dd').replace('%Y', 'yyyy')
+    return fmt
