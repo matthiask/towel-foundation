a3a715299b3206f3dd8456e0654c7bc5efe649fd
Matthias Kestenholz <mk@spinlock.ch>
2013-11-18 13:13:04 +0100

    Switch to setuptools

--- setup.py
+++ setup.py
@@ -1,23 +1,40 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
 import os
-import setuplib
+from setuptools import setup, find_packages
 
-packages, package_data = setuplib.find_packages('towel_foundation')
 
-setup(name='towel-foundation',
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+
+setup(
+    name='towel-foundation',
     version=__import__('towel_foundation').__version__,
     description='Keeping you DRY since 2013',
-    long_description=open(
-        os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    long_description=read('README.rst'),
     author='Matthias Kestenholz',
     author_email='mk@feinheit.ch',
     url='http://github.com/matthiask/towel-foundation/',
     license='BSD License',
     platforms=['OS Independent'],
-    packages=packages,
-    package_data=package_data,
+    packages=find_packages(),
+    package_data={
+        '': ['*.html', '*.txt'],
+        'towel_foundation': [
+            'locale/*/*/*.*',
+            'static/towel_foundation/*.*',
+            'static/towel_foundation/*/*.*',
+            'templates/*.*',
+            'templates/*/*.*',
+            'templates/*/*/*.*',
+            'templates/*/*/*/*.*',
+        ],
+    },
+    install_requires=[
+        'Django>=1.4.2',
+        'towel>=0.2.0',
+    ],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
@@ -26,8 +43,14 @@ setup(name='towel-foundation',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
     ],
+    zip_safe=False,
 )
